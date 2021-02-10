5621ba559595b948c9dc94bd852c5d2d46b952c9
Harsh Kaushik <kaushikharsh630@gmail.com>
2021-02-10 22:38:25 +0530

    corrected the error that shows up on counting beyond 999

--- app/src/main/java/com/example/simplecounter/MainActivity.java
+++ app/src/main/java/com/example/simplecounter/MainActivity.java
@@ -8,27 +8,33 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        }
 
-     int quantity=0;
-    public void increment(View view){
+        int quantity=990;
+public void increment(View view){
         //int quantity=0;
         quantity++;
         display(quantity);
-    }
-    public void decrement(View view){
+        }
+public void decrement(View view){
         //int quantity = 0;
         quantity--;
         display(quantity);
-    }
+        }
 
 
-    private void display(int number) {
+private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        if(quantity>999) {
+                quantityTextView.setTextSize(120);
+                quantityTextView.setText("" + number);
+        }
+        else
+                quantityTextView.setTextSize(160);
+                quantityTextView.setText("" + number);
     }
 }
